21c3748873991d60aa4e99da50afa813039e3dc5
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-01 15:10:47 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,11 @@
 def square_array(array)
   counter = 0
 
-
+while array[counter] do
+    array[counter] ** 2 
+    counter += 1
+  end
+end
 
 def square_array(array)
   array.each_with_object([]) { |i,arr| arr << i ** 2 }
