4184d820b2396db7a5b9caceefc166291e9f916c
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-01 15:03:36 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -6,18 +6,3 @@ def square_array(array)
     counter += 1
   end
 end
-
-number = [1, 2, 3]
-
-number = square_array(number)
-
-new_number = [9, 10, 16, 25]
-def square_array(array)
-  arr = []
-  array.each { |i| arr << i ** 2 }
-  arr
-end
-
-my_arr = [1, 2]
-p square_array(my_arr
-end
